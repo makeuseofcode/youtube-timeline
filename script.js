d46a046b4e075d05a3f29f2c12cd46198d24a870
Kingsley Ubah <ubahkingsley4@gmail.com>
2023-07-25 09:28:19 +0100

    adjust video player

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 const video = document.querySelector(".video")
 const timeline = document.querySelector(".timeline")
 
-/* 
-video.addEventListener("pointerdown", e=> {
-    const dot = document.createElement("div")
-    dot.classList.add(".dot")
-    dot.id = e.pointerId
-    positionDot(e, dot)
-    document.body.append(dot)
-  })
-
-  function positionDot(e, dot) {
-    dot.style.width = `${e.width * 10}px`
-    dot.style.height = `${e.height * 10}px`
-    dot.style.left = `${e.pageX}px`
-    dot.style.top = `${e.pageY}px`
-  }
-
-  video.addEventListener("pointermove", e=> {
-    const dot = document.getElementById(e.pointerId)
-    if(dot == null) return
-    positionDot(e, dot)
-  })
-*/
-
-// This is the actual YouTube player emulator:
-
   timeline.addEventListener("pointerdown", e=> {
     timeline.setPointerCapture(e.pointerId)
     setTimelinePosition(e)
